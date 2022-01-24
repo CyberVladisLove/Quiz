2374fa1b01f5d160fe5f9295549eae1fe2d5f82f
VladisLove <vlad.demidenko.93@mail.ru>
2022-01-24 11:27:05 +0400

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,15 +6,7 @@ export default class Main extends Component{
             <div>
                 Добро пожаловать в Квиз! Скорее начинай игру :)
 
-                ===Change1 for commit1===
-
-                ===Change2 for commit2===
-
-                ===Change3 for commit3===
-
-                ===Change4 for commit4==
-
-                ===Change5 for commit5==
+                
             </div>
             
         )
